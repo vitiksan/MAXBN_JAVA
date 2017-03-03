a31c9ed852e0eb28d8f62f96e7a90fef815263db
Vitalik <patriot02faqq@gmail.com>
2017-03-03 16:11:31 +0200

    Finish task 2001

--- src/main/java/com/maxGroup/Account.java
+++ src/main/java/com/maxGroup/Account.java
@@ -2,4 +2,53 @@ package com.maxGroup;
 
 
 public class Account {
+    private String numberAccount;
+    private int balans;
+    private int pass;
+
+    public Account(){
+        numberAccount = "0000000000";
+        balans = 0;
+        pass=11111111;
+    }
+    public Account(String numberAccount, int balans, int pass){
+        this.numberAccount=numberAccount;
+        this.balans=balans;
+        this.pass=pass;
+    }
+
+    public void fillBalans(int count){
+        balans+=count;
+    }
+    public Boolean GetMany(int count, int pass){
+        if(pass==this.pass && count <=this.balans)
+        {
+            balans-=count;
+            return true;
+        }
+        return false;
+    }
+    public String getNumberClient() {
+        return numberAccount;
+    }
+
+    public void setNumberClient(String numberAccount) {
+        this.numberAccount = numberAccount;
+    }
+
+    public int getBalans() {
+        return balans;
+    }
+
+    public void setBalans(int balans) {
+        this.balans = balans;
+    }
+
+    public int getPass() {
+        return pass;
+    }
+
+    public void setPass(int pass) {
+        this.pass = pass;
+    }
 }
