8d9ee0bdc8dc2c614c4d2f0e3c477d5fabc9277f
Vitalik <patriot02faqq@gmail.com>
2017-03-04 11:41:47 +0200

    Start task2002

--- src/main/java/com/maxGroup/Customer.java
+++ src/main/java/com/maxGroup/Customer.java
@@ -1,26 +1,22 @@
 package com.maxGroup;
 
 public class Customer extends Human {
+    private String idCustomer;
     private String statusCustomer; //gold, platinum or usual
     private int countCard;
     private int countCredit;
     private int countDeposit;
-    private String idCustomer;
-
 
     public Customer() {
         super();
-        statusCustomer = "usual";
-        countCard = 0;
-        countDeposit = 0;
-        countDeposit = 0;
-        idCustomer = "0000000";
+        this.statusCustomer = "usual";
+        this.idCustomer = "0000000";
     }
 
     public Customer(String statusCustomer, String idCustomer, String name, String surname, int year, int month, int day, int countCard, int countCredit, int countDeposit) {
         super(name, surname, year, month, day);
-        setStatusCustomer(statusCustomer);
-        setIdCustomer(idCustomer);
+        this.setIdCustomer(idCustomer);
+        this.statusCustomer=statusCustomer;
         setCountCard(countCard);
         setCountCredit(countCredit);
         setCountDeposit(countDeposit);
@@ -31,7 +27,7 @@ public class Customer extends Human {
         return getClass().getName() +
                 "[ name = " + getName()
                 + ", surname = " + getSurname()
-                + ", date born = " + born.getGregorianChange()
+                + ", date born = " + getBorn().getGregorianChange()
                 + ", IdCustomer = " + idCustomer
                 + ", StatusCustomer = " + statusCustomer
                 + "]";
