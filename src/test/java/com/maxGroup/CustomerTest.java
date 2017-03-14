f161dd0533606586034efa42ae77082e44dbaacc
Vitalik <patriot02faqq@gmail.com>
2017-03-14 17:20:13 +0200

    Finish Task4001

--- src/test/java/com/maxGroup/CustomerTest.java
+++ src/test/java/com/maxGroup/CustomerTest.java
@@ -1,5 +1,7 @@
 package com.maxGroup;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,6 +9,23 @@ import static org.junit.Assert.*;
  */
 public class CustomerTest {
     public static void informationExspertTest(String[] args) {
+        ArrayList<Customer> group = new ArrayList<Customer>();
+
+        group.add(new Customer("usual", "Vitalik", "Mah", 1997, 11, 29, 58000, 1111));
+        group.add(new Customer("usual", "Olia", "Stefanishin", 1999, 3, 22, 68000, 1221));
+        group.add(new Customer("usual", "Igor", "Kupchak", 1997, 5, 11, 36000, 3251));
+
+        int sum = 0;
+        for (Customer item : group) {
+            System.out.println(item.toString());
+            item.createAccount(2000, 12345);
+        }
+
+        for (Customer item : group) {
+            System.out.println(item.toString());
+            sum += item.getBalanceAccounts();
+        }
+        System.out.println("All sum accounts group: " +sum);
 
     }
 
