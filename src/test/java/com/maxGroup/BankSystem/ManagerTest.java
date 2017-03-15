22f1fdc194b3741ebb07c55eefa602d8bc355fd6
NAzarMh <nazar.myhailiv@gmail.com>
2017-03-15 13:02:47 +0200

    Task 5001

--- src/test/java/com/maxGroup/BankSystem/ManagerTest.java
+++ src/test/java/com/maxGroup/BankSystem/ManagerTest.java
@@ -18,21 +18,37 @@ public class ManagerTest {
         staff.add(new Consulter());
         staff.add(new Employee());
 
-        for (Employee human:staff){
+        for (Employee human : staff) {
             System.out.println(human.toString());
         }
 
-        for (Employee human:staff){
+        for (Employee human : staff) {
             human.riseSalary(500);
         }
 
         System.out.println("---------------");
 
-        for (Employee human:staff){
+        for (Employee human : staff) {
             System.out.println(human.toString());
         }
 
 
+    }
+
+    @Test //TODO Task 5001
+    public void setBonus() throws Exception {
+        ArrayList<Employee> staff = new ArrayList<Employee>();
+        staff.add(new Employee());
+        staff.add(new Manager());
+
+        int salary = 5000;
+
+        for (Employee human : staff) {
+            human.setSalary(salary);
+            human.setBonus();
+        }
+        assertEquals(salary * 1.10, staff.get(0).getSalary(), 0);
+        assertEquals(salary * 1.25, staff.get(1).getSalary(), 0);
     }
 
 }
