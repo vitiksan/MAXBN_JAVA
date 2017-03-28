fd2e4d28d8f762e028f6d837db201e7eb024ee91
Vitalik <patriot02faqq@gmail.com>
2017-03-28 16:06:40 +0300

    finish Task8001

--- src/main/java/com/maxGroup/GRASP/Sale.java
+++ src/main/java/com/maxGroup/GRASP/Sale.java
@@ -6,16 +6,24 @@ import java.util.GregorianCalendar;
 public class Sale {
     private GregorianCalendar date;
     private ArrayList<SaleItem> saleItems;
+    private int idSale;
+    private static int nextIdSale = 1;
 
 
     public Sale() {
         saleItems = new ArrayList<SaleItem>();
         date = new GregorianCalendar();
+        setIdSale();
     }
 
     public void addSaleItem(String descripton, double price, int q) {
 
         saleItems.add(new SaleItem(descripton, price, q));
+    }
+
+    public void setIdSale() {
+        idSale = nextIdSale;
+        nextIdSale++;
     }
 
     public ArrayList<SaleItem> getSaleItems() {
