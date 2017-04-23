bf3067db306154bc2e14c06af66e2babe591f5d5
atrik <patriot02faqq@gmail.com>
2017-04-23 18:13:57 +0300

    continue Task 16.01

--- src/main/java/com/maxGroup/Meeting/HomeTask16/HeapSort.java
+++ src/main/java/com/maxGroup/Meeting/HomeTask16/HeapSort.java
@@ -5,12 +5,15 @@ import org.apache.log4j.Logger;
 public class HeapSort<T extends Number> {
     private T[] arr;
     int size;
+    private int left;
+    private int right;
+    private int largest;
 
     private static final Logger log = Logger.getLogger(HeapSort.class);
 
     public HeapSort(T[] arr) {
         this.arr = arr;
-        this.size = arr.length;
+        this.size = arr.length-1;
     }
 
     private boolean replace(int x, int y) {
@@ -23,29 +26,4 @@ public class HeapSort<T extends Number> {
         return false;
     }
 
-    public T[] heapSort() {
-
-        for (int i = size / 2 - 1; i >= 0; i--) downHeap(i, size - 1);
-
-        for (int i = size - 1; i > 0; i--) {
-            replace(0, i);
-            downHeap(0, i - 1);
-        }
-        return arr;
-    }
-
-
-    private void downHeap(int index, int lim) {
-        T temp = arr[index];
-        int child;
-
-        while (index <= (size -1)/2) {
-            child = 2 * index;
-            if (child < lim && arr[child].doubleValue() < arr[child + 1].doubleValue())
-                child++;
-            if (temp.doubleValue() >= arr[child].doubleValue()) break;
-            replace(index,child);
-            index = child;
-        }
-    }
 }
