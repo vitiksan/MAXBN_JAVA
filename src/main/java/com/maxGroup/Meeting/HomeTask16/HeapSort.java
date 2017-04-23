efcb669fe3e7d285f8f4357210bba4d80e032ca0
atrik <patriot02faqq@gmail.com>
2017-04-23 17:00:31 +0300

    stop Task 16.01

--- src/main/java/com/maxGroup/Meeting/HomeTask16/HeapSort.java
+++ src/main/java/com/maxGroup/Meeting/HomeTask16/HeapSort.java
@@ -1,4 +1,55 @@
 package com.maxGroup.Meeting.HomeTask16;
 
-public class HeapSort {
+import org.apache.log4j.Logger;
+
+public class HeapSort<T extends Number> {
+    private T[] arr;
+    int size;
+
+    private static final Logger log = Logger.getLogger(HeapSort.class);
+
+    public HeapSort(T[] arr) {
+        this.arr = arr;
+        this.size = arr.length;
+    }
+
+    private boolean replace(int x, int y) {
+        if (x < size && y < size) {
+            T temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+            return true;
+        }
+        return false;
+    }
+
+
+    public T[] sort() {
+        int index = 0;
+        int temp = 0;
+        boolean replacing = false;
+        try {
+            while (size != 1) {
+                if (arr[index].doubleValue() < arr[2 * index + 1].doubleValue() && arr[2 * index + 1].doubleValue() > arr[2 * index + 2].doubleValue()) {
+                    replacing = replace(index, 2 * index + 1);
+                    if (!replacing) throw new Exception("can`t replace number");
+                } else if (arr[index].doubleValue() < arr[2 * index + 2].doubleValue() && arr[2 * index + 2].doubleValue() > arr[2 * index + 1].doubleValue()) {
+                    replacing = replace(index, 2 * index + 2);
+                    if (!replacing) throw new Exception("can`t replace number");
+                }
+
+                if (2 * index + 1 < size) index = 2 * index + 1;
+                else if (2 * index + 2 < size) index = 2 * index + 2;
+
+                if (replacing && index == 0) {
+                    replace(index, size - 1);
+                    size--;
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        return arr;
+    }
 }
---
+++ src/main/java/com/maxGroup/Meeting/HomeTask16/Runner.java
@@ -0,0 +1,12 @@
+package com.maxGroup.Meeting.HomeTask16;
+
+public class Runner {
+    public static void main(String[] args) {
+        Integer[] arr = {2, 5, 8, 2, 8, 5, 8, 4, 9, 4};
+        HeapSort heapSort = new HeapSort(arr);
+        arr = (Integer[]) heapSort.sort();
+        for (Integer i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
