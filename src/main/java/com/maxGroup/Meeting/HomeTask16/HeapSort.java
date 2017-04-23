f12d7e19f9f7ea1b86eaa7a6b49b3b4ccd276e3c
atrik <patriot02faqq@gmail.com>
2017-04-23 17:29:51 +0300

    stop Task 16.01

--- src/main/java/com/maxGroup/Meeting/HomeTask16/HeapSort.java
+++ src/main/java/com/maxGroup/Meeting/HomeTask16/HeapSort.java
@@ -25,23 +25,43 @@ public class HeapSort<T extends Number> {
 
     private boolean sortThreeElement(int index) {
         boolean replacing = false;
-        if (arr[index].doubleValue() < arr[2 * index + 1].doubleValue() && arr[2 * index + 1].doubleValue() > arr[2 * index + 2].doubleValue()) {
-            replacing = replace(index, 2 * index + 1);
-        } else if (arr[index].doubleValue() < arr[2 * index + 2].doubleValue() && arr[2 * index + 2].doubleValue() > arr[2 * index + 1].doubleValue()) {
-            replacing = replace(index, 2 * index + 2);
+        int left = 1;
+        int right = 2;
+        try {
+            if (arr[index].doubleValue() < arr[2 * index + 1].doubleValue() && arr[2 * index + 1].doubleValue() > arr[2 * index + 2].doubleValue()) {
+                replacing = replace(index, 2 * index + 1);
+                if (!replacing) throw new Exception();
+            } else if (arr[index].doubleValue() < arr[2 * index + 2].doubleValue() && arr[2 * index + 2].doubleValue() > arr[2 * index + 1].doubleValue()) {
+                replacing = replace(index, 2 * index + 2);
+                if (!replacing) throw new Exception();
+            }
+
+            left = 2 * index + 1;
+            right = 2 * index + 2;
+            if (2 * left + 1 < size) {
+                sortThreeElement(left);
+            } else if (2 * right + 2 < size) {
+                sortThreeElement(right);
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        } catch (Exception e) {
+            log.error("can`t replace");
+            log.error(e.getMessage());
+            e.printStackTrace();
         }
+        return replacing;
     }
 
 
     public T[] sort() {
         int index = 0;
-        int temp = 0;
         boolean replacing = false;
+
         while (size != 1) {
-
-            if (2 * index + 1 < size) index = 2 * index + 1;
-            else if (2 * index + 2 < size) index = 2 * index + 2;
-
+            sortThreeElement(index);
             if (replacing && index == 0) {
                 replace(index, size - 1);
                 size--;
