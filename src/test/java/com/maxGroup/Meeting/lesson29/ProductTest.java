034f6508713e235670071248e2306448118a1f71
atrik <patriot02faqq@gmail.com>
2017-07-04 14:31:09 +0300

    finish task 29.03

--- src/test/java/com/maxGroup/Meeting/lesson29/ProductTest.java
+++ src/test/java/com/maxGroup/Meeting/lesson29/ProductTest.java
@@ -2,10 +2,7 @@ package com.maxGroup.Meeting.lesson29;
 
 import org.junit.Test;
 
-import java.util.Comparator;
-import java.util.NavigableSet;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -27,6 +24,24 @@ public class ProductTest {
         sortByPrice.addAll(products);
 
         System.out.println(sortByPrice);
+    }
+
+    @Test
+    public void workWithPriorityQueue(){
+        PriorityQueue<Product> products = new PriorityQueue<Product>(Comparator.comparing(Product::getName));
+        products.add(new Product("beer", 25.2));
+        products.add(new Product("dark beer", 25.2));
+        products.add(new Product("cheace", 40.2));
+        products.add(new Product("nats", 15.2));
+        products.add(new Product("fish", 25.2));
+
+        System.out.println(products);
+
+        System.out.println(products.peek());
+
+        System.out.println(products.poll());
+        System.out.println(products.poll());
+        System.out.println(products.poll());
     }
 
 }
